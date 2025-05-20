36d4e90803ddaac4d840c2b4f163a129c99dab5a
hamasoron <hentaioekakiman@gmail.com>
2025-05-20 11:55:05 +0900

    20250520_1200

--- app/db-initdata/entrypoint.sh
+++ app/db-initdata/entrypoint.sh
@@ -7,7 +7,7 @@ set -e
 echo "[INFO] Starting DB initdata process..."
 
 # 環境変数の中身を確認（事前にECSのタスク定義のsecretsとenvironmentを設定しておく）
-REQUIRED_VARS="DB_HOST DB_PORT DB_NAME DB_MASTER_USERNAME DB_MASTER_PASSWORD"
+REQUIRED_VARS="DB_WRITER_HOST DB_PORT DB_NAME DB_MASTER_USERNAME DB_MASTER_PASSWORD"
 for VAR in $REQUIRED_VARS; do
   if [ -z "$(eval echo \$$VAR)" ]; then
     echo "[ERROR] Environment variable $VAR is not set."
@@ -24,7 +24,7 @@ fi
 # RDS/Auroraに接続できるかどうか疎通確認（最大30回リトライ）
 echo "[INFO] Waiting for database to be available..."
 for i in $(seq 1 30); do
-  if mysqladmin ping -h"$DB_HOST" -P"$DB_PORT" -u"$DB_MASTER_USERNAME" -p"$DB_MASTER_PASSWORD" --silent; then
+  if mysqladmin ping -h"$DB_WRITER_HOST" -P"$DB_PORT" -u"$DB_MASTER_USERNAME" -p"$DB_MASTER_PASSWORD" --silent; then
     echo "[INFO] Database is ready."
     break
   fi
@@ -33,14 +33,14 @@ for i in $(seq 1 30); do
 done
 
 # 30回試してもRDS/Aurora MySQLに接続できなかった場合は異常終了
-if ! mysqladmin ping -h"$DB_HOST" -P"$DB_PORT" -u"$DB_MASTER_USERNAME" -p"$DB_MASTER_PASSWORD" --silent; then
+if ! mysqladmin ping -h"$DB_WRITER_HOST" -P"$DB_PORT" -u"$DB_MASTER_USERNAME" -p"$DB_MASTER_PASSWORD" --silent; then
   echo "[ERROR] Database not reachable after 30 attempts."
   exit 1 ## 異常終了
 fi
 
 # 初期データ投入SQLの実行
 echo "[INFO] Executing initdata.sql..."
-mysql -h"$DB_HOST" -P"$DB_PORT" -u"$DB_MASTER_USERNAME" -p"$DB_MASTER_PASSWORD" "$DB_NAME" < /app/initdata.sql
+mysql -h"$DB_WRITER_HOST" -P"$DB_PORT" -u"$DB_MASTER_USERNAME" -p"$DB_MASTER_PASSWORD" "$DB_NAME" < /app/initdata.sql
 
 # 初期データ投入完了
 echo "[INFO] DB initdata completed successfully."
